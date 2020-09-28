4cc2b4ce2950d83dc14d3c92a599b492a2c3fc45
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-28 09:18:29 +0000

    Add 003-loops

---
+++ 003-loops/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _003_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("While looping");
+            int number = 0;
+            while(number <= 10)
+            {
+                if (number == 0)
+                {
+                    number++;
+                    continue;
+                }
+
+                Console.WriteLine(number);
+                number++;
+            }
+            Console.Read();
+
+            Console.WriteLine("Do While looping");
+            int number2 = 0;
+            do
+            {
+                if (number2 == 3)
+                {
+                    break;
+                }
+                Console.WriteLine(number2);
+                number2++;
+            }
+            while (number2 <= 10);
+            Console.Read();
+
+            Console.WriteLine("For looping");
+            for(int number3 = 15; number3 <= 30; number3++)
+            {
+                Console.WriteLine(number3);
+            }
+            Console.Read();
+
+            Console.WriteLine("Foreach looping");
+            int[] numberCollection = {1, 2, 3, 4, 5};
+            foreach(int value in numberCollection)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
