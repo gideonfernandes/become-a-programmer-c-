b27803cfa8454892b9336c83748f4ebdfba8aedb
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-10-01 09:56:11 +0000

    Add 005-functions

---
+++ 005-functions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _005_functions
+{
+    class Program
+    {
+        static int Calculate(int a, int b)
+        {
+            return a + b;
+        }
+        public static void PrintMessageOnScreen()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void CalculateTimes(int number)
+        {
+            int count = 1;
+            while(count <= 10)
+            {
+                Console.WriteLine(number + " X " + count + " = " + (number * count));
+                count++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate(15, 17));
+            Console.Read();
+            PrintMessageOnScreen();
+            Console.Read();
+            CalculateTimes(5);
+            CalculateTimes(15);
+        }
+    }
+}
