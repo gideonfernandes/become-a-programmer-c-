12c1e7db072c87fe40bb2f2e203ee388b0d86ad1
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-10-01 10:43:15 +0000

    Add 006-recursive-functions

---
+++ 006-recursive-functions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace _006_recursive_functions
+{
+    class Program
+    {
+        private static void ReadFiles(int fileNumber)
+        {
+            string filePath = @"./files/file" + fileNumber + ".txt";
+            if (File.Exists(filePath))
+            {
+                using(StreamReader file = File.OpenText(filePath))
+                {
+                    string row;
+                    while ((row = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(row);
+                    }
+                }
+            }
+
+            string nextFilePath = @"./files/file" + (fileNumber + 1) + ".txt";
+            if (File.Exists(nextFilePath))
+            {
+                ReadFiles(fileNumber + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ReadFiles(1);
+        }
+    }
+
+}
